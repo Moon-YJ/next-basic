980c8ef7cbabb3a024f3aa56bb4a9ec2e68fec33
Moon-YJ <tunjeong@gmail.com>
2024-01-10 15:51:47 +0900

    텍스트 split후 줄바꿈 적용하는 경우

--- app/post/[id]/page.jsx
+++ app/post/[id]/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function PostDetail({ params }) {
 	const [Post, setPost] = useState(null);
@@ -15,7 +15,22 @@ export default function PostDetail({ params }) {
 	return (
 		<div className='detail'>
 			<h2>{Post?.title}</h2>
-			<p>{Post?.body}</p>
+			{/* <p>
+				{Post?.body.split('\n').map(txt => {
+					return <p key={txt + idx}>{txt}</p>;
+				})}
+			</p> */}
+			<p>
+				{Post?.body.split('\n').map((txt) => {
+					return (
+						// <> 빈 fragment에 key값 넣어야할때 React 컴포넌트 호출하면 됨
+						<React.Fragment key={txt}>
+							{txt}
+							<br />
+						</React.Fragment>
+					);
+				})}
+			</p>
 		</div>
 	);
 }
