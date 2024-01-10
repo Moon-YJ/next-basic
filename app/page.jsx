3378143b8b096045deb71057abc72ec06ed9f5c7
Moon-YJ <tunjeong@gmail.com>
2024-01-10 12:59:59 +0900

    fill, sizes 속성으로 Image 컴포넌트 사이즈 지정

--- app/page.jsx
+++ app/page.jsx
@@ -16,11 +16,21 @@ export default function Home() {
 				<img src={pic.src} alt='image' />
 			</div> */}
 			<div className={clsx(styles.pic)}>
-				<Image src={pic2} alt='image' fill quality={75} />
+				{/* fill을 추가하거나 아니면 width, height 값 넣어줘야 next가 미리 값을 읽을 수 있음 */}
+				<Image src={pic2} alt='image' width={500} height={500} quality={75} />
 			</div>
 			<div className={clsx(styles.pic)}>
 				{/* priority 설정하면 먼저 랜더링됨 */}
-				<Image src={test} alt='image' fill quality={75} priority />
+				{/* lazy loading이 해제되므로 주의해야함, 보통 화면에 첫번째로 보이는 이미지에 priority속성 추가하는것을 권장 */}
+				<Image
+					src={test}
+					alt='image'
+					fill
+					quality={75}
+					// 속성이 fill일때는 next가 크기 산정이 어려우므로 특정 크기 미만의 브라우저 폭에 대한 사이즈 크기 지정 권장
+					// min-width: 808px이면 50vw, 아니면 100vw
+					sizes='(min-width: 808px) 50vw, 100vw'
+				/>
 			</div>
 		</main>
 	);
@@ -28,9 +38,15 @@ export default function Home() {
 
 /*
   Next에서 Image 컴포넌트를 사용해야하는 이유
+  - LCP(Largest Contentful Paint): 웹페이지 로딩속도 측정시 콘텐츠에 있는 이미지나 영상처럼 용량이 큰 소스까지 측정에 포함시키는 방식 (Image 컴포넌트는 LCP 성능점수를 높이기 위함)
+
   1. 원본 빌드시에 import한 뒤, 브라우저 크기에 따른 이미지 용량을 자동으로 최적화시키고
   2. 클라이언트 요청이 들어올때 미리 최적화시킨 이미지를 랜더링
   3. quality={}로 직접 최적화 가능 (0 ~ 100까지 설정 가능, default값은 75)
   4. priority 속성을 이용해서 빨리 출력해야되는 이미지에 우선순위 부여 가능 
   5. 기본적으로 Image 컴포넌트 lazy loading기능을 적용하는데 priority 설정하면 lazy loading 기능은 강제 해제됨 (주의)
+    --> lazy loading: viewport에 보이는 이미지만 랜더링을 하고, 문서에 있지만 viewport에 보이지 않으면 로딩을 미루다가 근접했을때 랜더링
+  6. Image 컴포넌트 호출시 가급적 width, height 속성을 지정해야 next가 빌드시 해당 사이즈에 맞는 최적화 작업을 할 수 있음
+  7. 이미지의 크기가 고정값이 아닐때에는 fill 속성 추가 (연결되어있는 style의 넓이, 높이를 판단해서 최적화)
+    --> fill 속성 적용시 추가적으로 하면 좋은 속성: sizes
 */
