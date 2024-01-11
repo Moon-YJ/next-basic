e7843ffca67f6419dd1a4e38a6947a821329efd2
Moon-YJ <tunjeong@gmail.com>
2024-01-11 11:12:12 +0900

    서버에 있는 배열값 응답 처리

--- components/postLists/postLists.jsx
+++ components/postLists/postLists.jsx
@@ -3,17 +3,27 @@
 import { useEffect, useState } from 'react';
 
 export default function PostLists() {
-	const [Posts, setPosts] = useState('');
+	const [Posts, setPosts] = useState([]);
 
 	useEffect(() => {
 		const fetchPost = async (url) => {
 			const data = await fetch(url, { method: 'GET' });
 			const json = await data.json();
-			setPosts(json);
-			console.log(json);
+			console.log(json.result);
+			setPosts(json.result);
 		};
 		fetchPost('/api/post');
 	}, []);
 
-	return <div className='postLists'></div>;
+	return (
+		<div className='postLists'>
+			{Posts.map((post) => {
+				return (
+					<article key={post.name}>
+						<h2>{post.name}</h2>
+					</article>
+				);
+			})}
+		</div>
+	);
 }
